a83b24de86d487da9e22c4b13f258d3f03da4a14
emanoel campos <68448029+emanoelcampos@users.noreply.github.com>
2023-04-24 00:04:39 -0300

    Update books.js

--- books.js
+++ books.js
@@ -1,16 +1,22 @@
 
-// nome-do-seu-arquivo-js
+// Defina a sua chave de API
+const API_KEY = "AIzaSyD0Y5WuRZs3KkXIUy6aoWqldTM4ErRyETM";
 
-const apiKey = 'AIzaSyD0Y5WuRZs3KkXIUy6aoWqldTM4ErRyETM';
-const bookId = '44CcDwAAQBAJ';
+// Defina o ID do livro que você quer obter as informações
+const BOOK_ID = "44CcDwAAQBAJ";
 
-fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}?key=${apiKey}`)
+// Faça uma requisição à API de livros do Google
+fetch(`https://www.googleapis.com/books/v1/volumes/${BOOK_ID}?key=${API_KEY}`)
   .then(response => response.json())
   .then(data => {
-    const bookTitle = data.volumeInfo.title;
-    document.getElementById('book').innerHTML = `<h2>Estou lendo:</h2><p>${bookTitle}</p>`;
-  })
-  .catch(error => {
-    console.error('Ocorreu um erro ao buscar o livro:', error);
+    // Extraia as informações que você precisa do objeto "data"
+    const title = data.volumeInfo.title;
+    const author = data.volumeInfo.authors[0];
+    const imageUrl = data.volumeInfo.imageLinks.thumbnail;
+  document.getElementById('book').innerHTML = `<h2>Estou lendo:</h2><p>${bookTitle}</p>`;
+    
+    // Use essas informações para exibir o livro em seu perfil do GitHub
+    // (por exemplo, com HTML e CSS)
   });
+
 
